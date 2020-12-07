83e289ff61d7e1bdf830725cf9cef2296d0cc505
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-12-07 10:59:36 -0800

    day 3

---
+++ 03/day_03.py
@@ -0,0 +1,100 @@
+import math
+import os
+
+from collections import namedtuple
+from typing import List
+
+
+Vector2 = namedtuple("Vector2", "x y")
+
+
+class TreeMap:
+    SQUARE_OPEN = "."
+    SQUARE_TREE = "#"
+
+    def __init__(self, input_file: str):
+        self.rows = list()
+        self.width = 0
+        self.path = ""
+
+        self._parse_input_file(input_file)
+
+    def _parse_input_file(self, input_file: str):
+        with open(input_file, 'r') as fp:
+            for line in fp.readlines():
+                row = line.strip()
+                self.rows.append(row)
+                if len(row) > self.width:
+                    self.width = len(row)
+
+    def get_square(self, row: List[str], position: int):
+        index = position
+        while index >= self.width:
+            index -= self.width
+        return row[index]
+
+    def tree_count(self, start_position: int, slope: Vector2) -> int:
+        path = ""
+
+        position = start_position
+        tree_count = 0
+        for row in self.rows[::slope.y]:
+            # Actual calculation here
+            square = self.get_square(row=row, position=position)
+            if square == self.SQUARE_TREE:
+                tree_count += 1
+
+            # Path for debugging
+            if square == self.SQUARE_TREE:
+                position_marker = "X"
+            else:
+                position_marker = "O"
+            row_str = row * math.ceil(position / self.width)
+            path += f"{row_str[0:position]}{position_marker}{row_str[position+1:]}\n"
+
+            # Increment position
+            position += slope.x
+
+        self.path = path
+        return tree_count
+
+
+def get_tree_count(slope: Vector2) -> int:
+    input_file = os.path.join(os.path.dirname(__file__), "input.txt")
+    output_file = os.path.join(os.path.dirname(__file__), f"output_x{slope.x}_y{slope.y}.txt")
+
+    try:
+        os.unlink(output_file)
+    except OSError:
+        pass
+
+    tree_map = TreeMap(input_file=input_file)
+    trees = tree_map.tree_count(start_position=0, slope=slope)
+
+    with open(output_file, 'w') as fp:
+        fp.write(tree_map.path)
+
+    return trees
+
+
+def main():
+    slopes = [
+        Vector2(1, 1),
+        Vector2(3, 1),
+        Vector2(5, 1),
+        Vector2(7, 1),
+        Vector2(1, 2),
+    ]
+
+    results = list()
+    for slope in slopes:
+        trees = get_tree_count(slope=slope)
+        results.append(trees)
+        print(f"Slope ({slope.x}, {slope.y}) Encountered {trees} trees.")
+
+    results_str = " * ".join(str(r) for r in results)
+    print(f"{results_str} = {math.prod(results)}")
+
+
+if __name__ == "__main__":
+    main()
