bc867f091a6424c2edd49b9638864814e416b140
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-12-14 15:36:00 -0800

    faster day 11

--- 2020/11/day_11.py
+++ 2020/11/day_11.py
@@ -1,143 +1,106 @@
 from __future__ import annotations
 import sys
-from enum import Enum
-from typing import List
+from typing import Generator, List, Tuple
 
 from utils.input_file import file_path_from_args
 from utils.read_lines import read_lines
+from utils.timer import Timer
 
 
-class State(Enum):
-    Floor = "."
-    Empty = "L"
-    Occupied = "#"
-
-    @staticmethod
-    def from_str(s: str) -> State:
-        for e in State:  # type: State
-            if e.value == s:
-                return e
+EMPTY = "L"
+OCCUPIED = "#"
+FLOOR = "."
 
 
 class Grid:
-    def __init__(self, file: str):
+    def __init__(self, input_file: str):
+        self.grid = []
         self.width = 0
         self.height = 0
-        self.rows = []
-        self._init_grid(file=file)
 
-    def __str__(self):
-        s = ""
-        for row in self.rows:
-            s += "  ".join([str(c) for c in row]) + "\n"
-        return s
+        self._updates = []
 
-    def reset(self):
-        for cell in self.cells():
-            if cell.state == State.Occupied:
-                cell.state = State.Empty
+        self._parse_input(file=input_file)
 
-    def _init_grid(self, file: str):
-        for j, line in enumerate(read_lines(file)):
-            row = []
-            for i, char in enumerate(line):
-                cell = Cell(x=i, y=j, state=State.from_str(char))
-                row.append(cell)
-            self.rows.append(row)
-        self.height = len(self.rows)
-        self.width = len(self.rows[0])
+    def _parse_input(self, file: str):
+        grid = []
+        for i, line in enumerate(read_lines(file), start=1):
+            grid.append([c for c in line])
+            self.height = i
+            self.width = len(line)
+        self.grid = grid
 
-    def get_cell(self, x: int, y: int) -> Cell or None:
-        if x in range(self.width) and y in range(self.height):
-            return self.rows[y][x]
-        else:
-            return None
+    @property
+    def occupied(self) -> int:
+        return [s for i, j, s in self.seats()].count(OCCUPIED)
 
-    def get_surrounding(self, cell: Cell) -> List[Cell]:
-        for j in range(cell.y-1, cell.y+2):
-            for i in range(cell.x-1, cell.x+2):
-                if (c := self.get_cell(i, j)) is not None:
-                    if c != cell:
-                        yield c
+    def seats(self) -> Generator[Tuple[int, int, str], None, None]:
+        for j in range(self.height):
+            for i in range(self.width):
+                yield i, j, self.grid[j][i]
 
-    def get_line_of_sight(self, cell: Cell) -> List[Cell]:
-        directions = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-        for i, j in directions:
-            x = cell.x + i
-            y = cell.y + j
-            while True:
-                c = self.get_cell(x, y)
-                if c is None:
-                    break
-                elif c.state == State.Floor:
-                    x += i
-                    y += j
-                    continue
-                else:
-                    break
+    def occupied_neighbors(self, x: int, y: int, skip_floor: bool, limit: int) -> int:
+        occupied = 0
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if not (dx == 0 and dy == 0):
+                    i = x + dx
+                    j = y + dy
+                    if i in range(self.width) and j in range(self.height) and not (i == x and j == y):
+                        if skip_floor is True:
+                            while i in range(0, self.width) and j in range(0, self.height) and self.grid[j][i] == FLOOR:
+                                i += dx
+                                j += dy
+                        if i in range(self.width) and j in range(self.height) and self.grid[j][i] == OCCUPIED:
+                            occupied += 1
+                            if occupied >= limit:
+                                return occupied
+        return occupied
 
-            if c is not None:
-                yield c
+    def set_seat(self, x: int, y: int, state: str):
+        self._updates.append((x, y, state))
 
-    def cells(self) -> List[Cell]:
-        for row in self.rows:
-            for cell in row:  # type: Cell
-                yield cell
-
-    def advance_round(self, neighbors_tolerated: int, search_method: str):
-        new_rows = []
-        for row in self.rows:
-            new_row = []
-            for cell in row:
-                if search_method == "neighbors":
-                    neighbor_states = [c.state for c in self.get_surrounding(cell)]
-                elif search_method == "line of sight":
-                    neighbor_states = [c.state for c in self.get_line_of_sight(cell)]
-                else:
-                    raise NotImplementedError(search_method)
-
-                if cell.state == State.Empty and State.Occupied not in neighbor_states:
-                    new_row.append(Cell(cell.x, cell.y, State.Occupied))
-                elif cell.state == State.Occupied and neighbor_states.count(State.Occupied) > neighbors_tolerated:
-                    new_row.append(Cell(cell.x, cell.y, State.Empty))
-                else:
-                    new_row.append(cell)
-            new_rows.append(new_row)
-
-        self.rows = new_rows
-
-
-class Cell:
-    def __init__(self, x: int, y: int, state: State):
-        self.x = x
-        self.y = y
-        self.state = state
-
-    def __str__(self):
-        return self.state.value
+    def update(self):
+        for i, j, seat in self._updates:
+            self.grid[j][i] = seat
+        self._updates = []
 
 
 def main(input_file: str) -> int:
-    grid = Grid(file=input_file)
-    stabilize(grid=grid, leave_at=4, search_method="neighbors")
-    stabilize(grid=grid, leave_at=5, search_method="line of sight")
+    grid = Grid(input_file=input_file)
+    with Timer():
+        stabilize(grid=grid, skip_floor=False, occupied_threshold=4)
+
+    grid = Grid(input_file=input_file)
+    with Timer():
+        stabilize(grid=grid, skip_floor=True, occupied_threshold=5)
+
     return 0
 
 
-def stabilize(grid: Grid, leave_at: int, search_method: str):
-    grid.reset()
-    previous_state = None
-    counter = 0
-    while (current_state := str(grid)) != previous_state:
-        previous_state = current_state
-        grid.advance_round(neighbors_tolerated=leave_at-1, search_method=search_method)
-        counter += 1
+def stabilize(grid: Grid, skip_floor: bool, occupied_threshold: int):
+    stabilized = False
+    num_rounds = 0
 
-    stabilization = counter - 1
-    occupied = len([c for c in grid.cells() if c.state == State.Occupied])
+    while stabilized is False:
+        num_rounds += 1
+        stabilized = True
 
-    print(f"Seats stabilized after {stabilization} rounds (search method: {search_method}).")
-    print(f"{occupied} seats are occupied.")
+        for i, j, seat in grid.seats():
+            if seat == FLOOR:
+                continue
+            elif seat == EMPTY:
+                occupied = grid.occupied_neighbors(x=i, y=j, skip_floor=skip_floor, limit=1)
+                if occupied == 0:
+                    grid.set_seat(x=i, y=j, state=OCCUPIED)
+                    stabilized = False
+            elif seat == OCCUPIED:
+                occupied = grid.occupied_neighbors(x=i, y=j, skip_floor=skip_floor, limit=occupied_threshold)
+                if occupied >= occupied_threshold:
+                    grid.set_seat(x=i, y=j, state=EMPTY)
+                    stabilized = False
+        grid.update()
+    print(f"Seats stabilized after {num_rounds} rounds. {grid.occupied} seats are occupied")
 
 
 if __name__ == "__main__":
